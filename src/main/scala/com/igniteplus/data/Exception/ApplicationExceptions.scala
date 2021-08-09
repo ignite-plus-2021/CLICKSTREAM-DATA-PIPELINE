fdb566409bcd436a7f9bd5cb16796b3cc5828fc9
Malikajaiswal <68912714+Malikajaiswal@users.noreply.github.com>
2021-08-09 20:12:37 +0530

    Create ApplicationExceptions.scala

---
+++ src/main/scala/com/igniteplus/data/Exception/ApplicationExceptions.scala
@@ -0,0 +1,12 @@
+package com.igniteplus.data.pipeline.Exception
+
+
+  class ApplicationExceptions(message: String, cause: Throwable) extends Exception(message,cause){
+    def this(message: String) = this(message, None.orNull)
+  }
+
+  case class FileReadException (message: String) extends ApplicationExceptions(message)
+
+  case class FileWriteException (message: String) extends ApplicationExceptions(message)
+
+
