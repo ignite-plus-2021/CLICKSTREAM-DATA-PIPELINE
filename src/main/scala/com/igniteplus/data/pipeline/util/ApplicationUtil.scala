fc2ebf09823f5e14bd5965628e3275904a083ae3
Gunpreet <1999gunpreet@gmail.com>
2021-08-09 20:02:11 +0530

    Removed spark.conf and Application Utils

--- src/main/scala/com/igniteplus/data/pipeline/util/ApplicationUtil.scala
+++ src/main/scala/com/igniteplus/data/pipeline/util/ApplicationUtil.scala
@@ -1,31 +1,5 @@
 package com.igniteplus.data.pipeline.util
 
 object ApplicationUtil{
-<<<<<<< HEAD
 
-  /**
-   * READING SPARK CONFIGURATIONS FROM A SEPERATE FILE
-   * @param fileName where you've mentioned the spark configuration
-   * @return sparkConf object which can be passed as a parameter to  createSparkSession
-   */
-  def getSparkConf(fileName: String): SparkConf ={
-    val sparkAppConf = new SparkConf()
-    val props = new Properties()
-    props.load(Source.fromFile(fileName).bufferedReader())
-    props.asScala.foreach(kv => sparkAppConf.set(kv._1,kv._2))
-    sparkAppConf
-  }
-
-  /**
-   * SPARK SESSION CREATION
-   * @param sparkConfiguration
-   * @return spark
-   */
-  def createSparkSession(sparkConfiguration:SparkConf):SparkSession = {
-    implicit val spark:SparkSession = SparkSession.builder().config(sparkConfiguration).getOrCreate()
-    spark
-  }
-
-=======
->>>>>>> parent of dddd6f6 (Added spark.conf file and updated ApplicationUtil)
 }
