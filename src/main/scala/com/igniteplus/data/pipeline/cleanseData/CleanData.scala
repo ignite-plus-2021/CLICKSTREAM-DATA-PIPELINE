0cc7b6934f0a624623e3891bdd1a89086cf0fa01
Harshita Saxena <64150164+har882@users.noreply.github.com>
2021-08-11 15:24:25 +0530

    updated trimColumns

--- src/main/scala/com/igniteplus/data/pipeline/cleanseData/CleanData.scala
+++ src/main/scala/com/igniteplus/data/pipeline/cleanseData/CleanData.scala
@@ -2,20 +2,44 @@ package com.igniteplus.data.pipeline.cleanseData
 
 
 import com.igniteplus.data.pipeline.constants.ApplicationConstants.{ROW_CONDITION, ROW_NUMBER}
-import com.igniteplus.data.pipeline.service.FileWriterService.writeFile
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, initcap, lower}
+import org.apache.spark.sql.functions.{col, desc, lower, row_number, trim, unix_timestamp}
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{col, desc, row_number}
-
 import com.igniteplus.data.pipeline.constants.ApplicationConstants.{TIMESTAMP_DATATYPE, TTIMESTAMP_FORMAT}
 import com.igniteplus.data.pipeline.service.FileWriterService.writeFile
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, unix_timestamp}
 
 
 
 object CleanData {
+
+  def dataTypeValidation(df:DataFrame,colName:Seq[String], dt:Seq[String]): DataFrame = {
+    /**
+     * Function to to change the data type of the columns to correct datatype
+     * @param df the dataframe
+     * @param colName sequence of columns of the df dataframe
+     * @param dt sequence of data types
+     * @return dataframe with updated data type
+     */
+    var dfChangedDataType = df
+    for (i <- colName.indices) {
+      if (dt(i) == TIMESTAMP_DATATYPE)
+        dfChangedDataType = dfChangedDataType.withColumn(colName(i), unix_timestamp(col(colName(i)), TTIMESTAMP_FORMAT).cast(TIMESTAMP_DATATYPE))
+      else
+        dfChangedDataType = dfChangedDataType.withColumn(colName(i), col(colName(i)).cast(dt(i)))
+    }
+    dfChangedDataType
+  }
+
+
+  /** @param df the dataframe taken as an input
+   *  @return dataframe with no whitespaces */
+  def trimColumn(df: DataFrame): DataFrame = {
+    var trimmedDF: DataFrame = df
+    for (n <- df.columns) trimmedDF = df.withColumn(n, trim(col(n)))
+    trimmedDF.show()
+    trimmedDF
+  }
+
   /**
    * Function to remove and filter null values and write null values to separate file
    * @param df             the dataframe taken as an input
@@ -43,54 +67,65 @@ object CleanData {
    * @param orderByColumn
    * @return dataframe with no duplicates
    */
-  def removeDuplicates (df:DataFrame , primaryKeyColumns : Seq[String], orderByColumn: Option[String]) : DataFrame  =
-  {
-    val dfDropDuplicates: DataFrame = orderByColumn match {
-      case Some(orderCol) => {
-        val windowSpec = Window.partitionBy(primaryKeyColumns.map(col): _*).orderBy(desc(orderCol))
-        df.withColumn(colName = ROW_NUMBER, row_number().over(windowSpec))
-          .filter(conditionExpr = ROW_CONDITION).drop(ROW_NUMBER)
-  }
-      case _ => df.dropDuplicates(primaryKeyColumns)
-  }
-  dfDropDuplicates
-  }
 
-  /**
-   * Function to to change the data type of the columns to correct datatype
-   * @param df the dataframe
-   * @param colName sequence of columns of the df dataframe
-   * @param dt sequence of data types
-   * @return dataframe with updated data type
-   */
-  def dataTypeValidation(df:DataFrame,colName:Seq[String], dt:Seq[String]): DataFrame = {
-    var dfChangedDataType = df
-    for (i <- colName.indices) {
-      if (dt(i) == TIMESTAMP_DATATYPE)
-        dfChangedDataType = dfChangedDataType.withColumn(colName(i), unix_timestamp(col(colName(i)), TTIMESTAMP_FORMAT).cast(TIMESTAMP_DATATYPE))
-      else
-        dfChangedDataType = dfChangedDataType.withColumn(colName(i), col(colName(i)).cast(dt(i)))
+  def removeDuplicates (df:DataFrame , primaryKeyColumns : Seq[String], orderByColumn: Option[String]) : DataFrame  = {
+
+    def removeDuplicates(df: DataFrame,
+                         primaryKeyColumns: Seq[String],
+                         orderByColumn: Option[String]
+                        ): DataFrame = {
+
+
+      val dfDropDuplicates: DataFrame = orderByColumn match {
+        case Some(orderCol) => {
+          val windowSpec = Window.partitionBy(primaryKeyColumns.map(col): _*).orderBy(desc(orderCol))
+          df.withColumn(colName = ROW_NUMBER, row_number().over(windowSpec))
+            .filter(conditionExpr = ROW_CONDITION).drop(ROW_NUMBER)
+        }
+        case _ => df.dropDuplicates(primaryKeyColumns)
+      }
+      dfDropDuplicates
     }
-    dfChangedDataType
-  }
 
-  /**
-   * CONVERT TO LOWER CASE
-   * @param inputDF
-   * @param columnToBeModified
-   * @param columnToBeNamed
-   * @return a modified Dataframe with modified case
-   */
-    def toLowerCase(df:DataFrame, columnTobeModified:Seq[String]):DataFrame = {
-    var dfLowerCase:DataFrame = df
-    for(i <- columnTobeModified)
-    {
-      dfLowerCase = dfLowerCase.withColumn(df(i).toString(), lower(col(df(i).toString())))
+    /**
+     * Function to to change the data type of the columns to correct datatype
+     *
+     * @param df      the dataframe
+     * @param colName sequence of columns of the df dataframe
+     * @param dt      sequence of data types
+     * @return dataframe with updated data type
+     */
+    def dataTypeValidation(df: DataFrame, colName: Seq[String], dt: Seq[String]): DataFrame = {
+      var dfChangedDataType = df
+      for (i <- colName.indices) {
+        if (dt(i) == TIMESTAMP_DATATYPE)
+          dfChangedDataType = dfChangedDataType.withColumn(colName(i), unix_timestamp(col(colName(i)), TTIMESTAMP_FORMAT).cast(TIMESTAMP_DATATYPE))
+        else
+          dfChangedDataType = dfChangedDataType.withColumn(colName(i), col(colName(i)).cast(dt(i)))
+      }
+      dfChangedDataType
     }
-    dfLowerCase
+
+
+    /**
+     * CONVERT TO LOWER CASE
+     *
+     * @param inputDF
+     * @param columnToBeModified
+     * @param columnToBeNamed
+     * @return a modified Dataframe with modified case
+     */
+    def toLowerCase(df: DataFrame, columnTobeModified: Seq[String]): DataFrame = {
+      var dfLowerCase: DataFrame = df
+      for (i <- columnTobeModified) {
+        dfLowerCase = dfLowerCase.withColumn(df(i).toString(), lower(col(df(i).toString())))
+      }
+      dfLowerCase
+    }
+
   }
 
-}
+
 
 
 
