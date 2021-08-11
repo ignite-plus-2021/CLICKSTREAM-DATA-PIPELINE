ece4a8e21af5ebd2de9107ebe542b15aef774e22
Pranjal Gandhi <nicepranjal@gmail.com>
2021-08-11 17:32:37 +0530

    Changes made

--- src/main/scala/com/igniteplus/data/pipeline/service/FileWriterService.scala
+++ src/main/scala/com/igniteplus/data/pipeline/service/FileWriterService.scala
@@ -7,14 +7,14 @@ object FileWriterService {
    * Writes the content specified to a file
    * @param df the dataframe that needs to be written to a file
    * @param fileType specified the format of the file
-   * @param filePath specifies where the file is located
+   * @param outputPath specifies where the file is located
    */
-  def writeFile(df:DataFrame, fileType:String, filePath:String) : Unit =
+  def writeFile(df:DataFrame, fileType:String, outputPath:String) : Unit =
   {
     df.write.format(fileType)
       .option("header", "true")
       .mode("overwrite")
       .option("sep", ",")
-      .save(filePath)
+      .save(outputPath)
   }
 }
