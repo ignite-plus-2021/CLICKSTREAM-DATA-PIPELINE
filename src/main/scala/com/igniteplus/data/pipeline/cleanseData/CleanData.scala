0e04d7b0fb7146916b9322107628f4d19dbd0976
Gunpreet <1999gunpreet@gmail.com>
2021-08-11 15:06:19 +0530

    Convert to Lower Case function added

--- src/main/scala/com/igniteplus/data/pipeline/cleanseData/CleanData.scala
+++ src/main/scala/com/igniteplus/data/pipeline/cleanseData/CleanData.scala
@@ -2,28 +2,48 @@ package com.igniteplus.data.pipeline.cleanseData
 
 import com.igniteplus.data.pipeline.service.FileWriterService.writeFile
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.{col, initcap, lower}
 
-object CleanData
-{
+object CleanData {
   /**
    * Function to remove and filter null values and write null values to separate file
-   * @param df the dataframe taken as an input
+   *
+   * @param df             the dataframe taken as an input
    * @param primaryColumns sequence of primary key columns
-   * @param filePath the location where null values will be written
-   * @param fileFormat specifies format of the file
+   * @param filePath       the location where null values will be written
+   * @param fileFormat     specifies format of the file
    * @return notNullDf which is the data free from null values
    */
-  def filterRemoveNull(df : DataFrame, primaryColumns : Seq[String], filePath : String, fileFormat : String) : DataFrame = {
-    var nullDf : DataFrame = df
-    var notNullDf : DataFrame = df
-    for( i <- primaryColumns)
-    {
+  def filterRemoveNull(df: DataFrame, primaryColumns: Seq[String], filePath: String, fileFormat: String): DataFrame = {
+    var nullDf: DataFrame = df
+    var notNullDf: DataFrame = df
+    for (i <- primaryColumns) {
       nullDf = df.filter(df(i).isNull)
       notNullDf = df.filter(df(i).isNotNull)
     }
-    if(nullDf.count() > 0)
+    if (nullDf.count() > 0)
       writeFile(nullDf, fileFormat, filePath)
     notNullDf
   }
 
+  /**
+   * CONVERT TO LOWER CASE
+   * @param inputDF
+   * @param columnToBeModified
+   * @param columnToBeNamed
+   * @return a modified Dataframe with modified case
+   */
+  def convertToLowerCase(inputDF: DataFrame, columnToBeModified: String, columnToBeNamed: String): DataFrame = {
+    val consistentNames: DataFrame = inputDF.withColumn(columnToBeNamed, lower(col(columnToBeModified)))
+    consistentNames
+  }
 }
+
+
+
+
+
+
+
+
+
