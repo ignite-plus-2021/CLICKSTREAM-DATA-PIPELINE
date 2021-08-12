4810cc4af15cb35dff305f823c69e1d9bbd1db13
Harshita Saxena <64150164+har882@users.noreply.github.com>
2021-08-12 21:23:33 +0530

    deleted CleanData file

--- src/main/scala/com/igniteplus/data/pipeline/cleanse/Cleanser.scala
+++ src/main/scala/com/igniteplus/data/pipeline/cleanse/Cleanser.scala
@@ -1,25 +1,24 @@
 package com.igniteplus.data.pipeline.cleanse
 
 
-import com.igniteplus.data.pipeline.constants.ApplicationConstants.{ROW_CONDITION, ROW_NUMBER}
-import org.apache.spark.sql.functions.{col, desc, lower, row_number, trim, unix_timestamp}
-import org.apache.spark.sql.expressions.Window
-import com.igniteplus.data.pipeline.constants.ApplicationConstants.{TIMESTAMP_DATATYPE, TTIMESTAMP_FORMAT}
+import com.igniteplus.data.pipeline.constants.ApplicationConstants.{ROW_NUMBER, TIMESTAMP_DATATYPE, TTIMESTAMP_FORMAT}
 import com.igniteplus.data.pipeline.service.FileWriterService.writeFile
 import org.apache.spark.sql.DataFrame
-
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 
 
 object Cleanser {
 
   /**
    * Function to to change the data type of the columns to correct datatype
+   *
    * @param df the dataframe
    * @param columnNames sequence of columns of the df dataframe
-   * @param dataTypes sequence of data types
+   * @param dataTypes   sequence of data types
    * @return dataframe with updated data type
    */
-  def dataTypeValidation(df:DataFrame,columnNames:Seq[String], dataTypes:Seq[String]): DataFrame = {
+  def dataTypeValidation(df: DataFrame, columnNames: Seq[String], dataTypes: Seq[String]): DataFrame = {
     var dfChangedDataType = df
     for (i <- columnNames.indices) {
       if (dataTypes(i) == TIMESTAMP_DATATYPE)
@@ -32,7 +31,7 @@ object Cleanser {
 
 
   /** @param df the dataframe taken as an input
-   *  @return dataframe with no whitespaces */
+   * @return dataframe with no whitespaces */
   def trimColumn(df: DataFrame): DataFrame = {
     var trimmedDF: DataFrame = df
     for (n <- df.columns) trimmedDF = df.withColumn(n, trim(col(n)))
@@ -42,16 +41,17 @@ object Cleanser {
 
   /**
    * Function to remove and filter null values and write null values to separate file
+   *
    * @param df             the dataframe taken as an input
-   * @param primaryColumns sequence of primary key columns
+   * @param primaryKeyColumns sequence of primary key columns
    * @param filePath       the location where null values will be written
    * @param fileFormat     specifies format of the file
    * @return notNullDf which is the data free from null values
    */
-  def filterRemoveNull(df: DataFrame, primaryColumns: Seq[String], filePath: String, fileFormat: String): DataFrame = {
+  def filterRemoveNull(df: DataFrame, primaryKeyColumns: Seq[String], filePath: String, fileFormat: String): DataFrame = {
     var nullDf: DataFrame = df
     var notNullDf: DataFrame = df
-    for (i <- primaryColumns) {
+    for (i <- primaryKeyColumns) {
       nullDf = df.filter(df(i).isNull)
       notNullDf = df.filter(df(i).isNotNull)
     }
@@ -60,49 +60,48 @@ object Cleanser {
     notNullDf
   }
 
+
   /**
    * Function to remove duplicates from the data
+   *
    * @param df the dataframe
    * @param primaryKeyColumns sequence of primary key columns of the df dataframe
    * @param orderByColumn
    * @return dataframe with no duplicates
    */
-    
-    def removeDuplicates(df: DataFrame,
-                         primaryKeyColumns: Seq[String],
-                         orderByColumn: Option[String]
-                        ): DataFrame = {
+  def removeDuplicates(df: DataFrame,
+                       primaryKeyColumns: Seq[String],
+                       orderByColumn: Option[String]
+                      ): DataFrame = {
 
-
-      val dfDropDuplicates: DataFrame = orderByColumn match {
-        case Some(orderCol) => {
-          val windowSpec = Window.partitionBy(primaryKeyColumns.map(col): _*).orderBy(desc(orderCol))
-          df.withColumn(colName = ROW_NUMBER, row_number().over(windowSpec))
-            .filter(conditionExpr = ROW_CONDITION).drop(ROW_NUMBER)
-        }
-        case _ => df.dropDuplicates(primaryKeyColumns)
+    val dfDropDuplicates: DataFrame = orderByColumn match {
+      case Some(orderCol) => {
+        val windowSpec = Window.partitionBy(primaryKeyColumns.map(col): _*).orderBy(desc(orderCol))
+        df.withColumn(colName = ROW_NUMBER, row_number().over(windowSpec))
+          .filter(col(ROW_NUMBER) === 1).drop(ROW_NUMBER)
       }
-      dfDropDuplicates
+      case _ => df.dropDuplicates(primaryKeyColumns)
     }
-
-
-
-
-    /**
-     * CONVERT TO LOWER CASE
-     * @param df
-     * @param columnToBeModified
-     * @return a modified Dataframe with modified case
-     */
-    def toLowerCase(df: DataFrame, columnTobeModified: Seq[String]): DataFrame = {
-      var dfLowerCase: DataFrame = df
-      for (columnToModify <- columnTobeModified) {
-        dfLowerCase = dfLowerCase.withColumn(df(columnToModify).toString(),lower(col(df(columnToModify).toString())))
-      }
-      dfLowerCase
-    }
-
+    dfDropDuplicates
   }
+
+
+  /**
+   * CONVERT TO LOWER CASE
+   *
+   * @param df the dataframe
+   * @param columnTobeModified Seq of columns
+   * @return a modified Dataframe with modified case
+   */
+  def toLowerCase(df: DataFrame, columnTobeModified: Seq[String]): DataFrame = {
+    var dfLowerCase: DataFrame = df
+    for (columnToModify <- columnTobeModified) {
+      dfLowerCase = dfLowerCase.withColumn(df(columnToModify).toString(), lower(col(df(columnToModify).toString())))
+    }
+    dfLowerCase
+  }
+
+}
 
 
 
