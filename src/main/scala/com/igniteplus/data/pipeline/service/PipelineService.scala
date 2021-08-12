a036b312cfe5dfd07e22012e0c47efe5893874df
Gunpreet <1999gunpreet@gmail.com>
2021-08-12 18:18:44 +0530

    error resolved

--- src/main/scala/com/igniteplus/data/pipeline/service/PipelineService.scala
+++ src/main/scala/com/igniteplus/data/pipeline/service/PipelineService.scala
@@ -1,7 +1,7 @@
 package com.igniteplus.data.pipeline.service
 
 import com.igniteplus.data.pipeline.cleanse.Cleanser.{dataTypeValidation, filterRemoveNull, removeDuplicates, toLowerCase, trimColumn}
-import com.igniteplus.data.pipeline.constants.ApplicationConstants.{CLICKSTREAM_DATASET, CLICKSTREAM_NULL_ROWS_DATASET_PATH, COLUMNS_LOWERCASE_CLICKSTREAM, COLUMNS_LOWERCASE_ITEM, COLUMNS_PRIMARY_KEY_CLICKSTREAM, COLUMNS_PRIMARY_KEY_ITEM, COLUMNS_VALID_DATATYPE_CLICKSTREAM, COLUMNS_VALID_DATATYPE_ITEM, EVENT_TIMESTAMP_OPTION, ITEM_DATASET, ITEM_NULL_ROWS_DATASET_PATH, NEW_DATATYPE_CLICKSTREAM, NEW_DATATYPE_ITEM, READ_FORMAT, SPARK_CONF, WRITE_FORMAT}
+import com.igniteplus.data.pipeline.constants.ApplicationConstants._
 import com.igniteplus.data.pipeline.service.FileReaderService.readFile
 import com.igniteplus.data.pipeline.util.ApplicationUtil.createSparkSession
 import org.apache.spark.sql.DataFrame
@@ -19,6 +19,7 @@ object PipelineService
     val itemDataDf: DataFrame = readFile(ITEM_DATASET, READ_FORMAT)
 
 
+
     /************************** CHANGE DATATYPE *****************************************************/
     val changedDatatypeClickStreamDataDf = dataTypeValidation(clickStreamDataDf, COLUMNS_VALID_DATATYPE_CLICKSTREAM,NEW_DATATYPE_CLICKSTREAM)
     val changedDatatype = dataTypeValidation(itemDataDf, COLUMNS_VALID_DATATYPE_ITEM, NEW_DATATYPE_ITEM)
@@ -31,11 +32,11 @@ object PipelineService
 
     /***************** NULL VALUE CHECKING *********************************************************** */
 
-    val nullValueCheckClickStreamDf: DataFrame = filterRemoveNull(clickStreamDataDf, COLUMNS_PRIMARY_KEY_CLICKSTREAM, CLICKSTREAM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
+    val nullValueCheckClickStreamDataDf: DataFrame = filterRemoveNull(clickStreamDataDf, COLUMNS_PRIMARY_KEY_CLICKSTREAM, CLICKSTREAM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
     val nullValueCheckItemDf: DataFrame = filterRemoveNull(itemDataDf, COLUMNS_PRIMARY_KEY_ITEM, ITEM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
 
-    val nullValueCheckClickStreamDataDf: DataFrame = filterRemoveNull(trimmedClickStreamDataDf, COLUMNS_PRIMARY_KEY_CLICKSTREAM, CLICKSTREAM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
-    val nullValueCheckItemDf: DataFrame = filterRemoveNull(trimmedItemDf, COLUMNS_PRIMARY_KEY_ITEM, ITEM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
+//    val nullValueCheckClickStreamDataDf: DataFrame = filterRemoveNull(trimmedClickStreamDataDf, COLUMNS_PRIMARY_KEY_CLICKSTREAM, CLICKSTREAM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
+//    val nullValueCheckItemDf: DataFrame = filterRemoveNull(trimmedItemDf, COLUMNS_PRIMARY_KEY_ITEM, ITEM_NULL_ROWS_DATASET_PATH, WRITE_FORMAT)
 
     /**************************** DEDUPLICAION *******************************************************************/
     val dedupliactedClickStreamDataDf = removeDuplicates(nullValueCheckClickStreamDataDf,COLUMNS_PRIMARY_KEY_CLICKSTREAM,Some(EVENT_TIMESTAMP_OPTION))
