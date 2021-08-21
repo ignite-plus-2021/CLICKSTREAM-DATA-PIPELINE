3ea63e7384b1cc3860c63d14253af3b3637d4fdb
Gunpreet <1999gunpreet@gmail.com>
2021-08-21 17:32:03 +0530

    File Reader Service Test Case Updated

---
+++ src/test/scala/com/igniteplus/data/pipeline/Helper/Helper.scala
@@ -0,0 +1,11 @@
+package com.igniteplus.data.pipeline.Helper
+
+trait Helper {
+
+  /* Helpers for File Reader Service Test Case */
+  val READ_LOCATION = "data/Test_Inputs/FileReaderServiceTestCaseInput.csv"
+  val FILE_FORMAT = "csv"
+  val COUNT_SHOULD_BE = 4
+  val READ_WRONG_LOCATION = "data/Test_Inputs/FileReaderServiceTestCaseInp.csv"
+
+}
